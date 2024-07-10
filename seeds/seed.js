5100b8948968bd2e612495afdbb28053715b3e97
TravisJBlack <159502505+TravisJBlack@users.noreply.github.com>
2024-07-10 16:15:15 -0500

    jj

--- seeds/seed.js
+++ seeds/seed.js
@@ -2,11 +2,11 @@ const sequelize = require('../config/connection');
 const { User, Blog, Comment } = require('../models');
 
 const userdata = require('./userData.json');
-const blogData = require('./blogData');
+const blogData = require('./blogData.json');
 const commentData = require('./commentData.json');
 
 const seedDatabase = async () => {
-    await sequelize.sync({ force: true });
+    await sequelize.sync({ force: false });
 
         const user = await User.bulkCreate(userdata, {
             individualHooks: true,
